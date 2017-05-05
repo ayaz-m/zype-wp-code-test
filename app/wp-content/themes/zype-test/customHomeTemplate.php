ff8fb9144d320ed76fe0d927c07fa978ef110562
Ayaz M <ayaz.m@allshoreresources.com>
2017-05-06 02:53:15 +0500

    Full functional repo

---
+++ app/wp-content/themes/zype-test/customHomeTemplate.php
@@ -0,0 +1,97 @@
+<?php
+/* 
+ * Template Name: Home Template 
+ */
+/**
+ * @package Zype_Test
+ */
+get_header(); ?>
+
+	<div id="primary" class="content-area container">
+		<main id="main" class="site-main" role="main">
+                    <?php
+			while ( have_posts() ) : the_post();
+                     ?>
+                        <div class="col-lg-12 col-sm-12 col-md-12"><h1 class="text-center"> <?php the_title(); ?></h1></div>
+                        <div class="col-lg-12 col-sm-12 col-md-12 text-center"><h2>Posts</h2></div>
+                
+                    <?php
+                            // The Query
+                             $args = array(
+                                'post_type' => 'post',
+                                'posts_per_page' => 3
+                            );
+                            $posts = new WP_Query( $args );
+                            // The Loop
+                                if ( $posts->have_posts() ) :?>
+                                      <div class='col-lg-12 col-sm-12 col-md-12'>
+                                    <?php    while ( $posts->have_posts() ): $posts->the_post();
+                                                ?>
+                                                <div class='col-lg-4 col-sm-4 col-md-4'>
+                                                    <h4> <?php echo get_the_title();?> </h4>
+                                                    <p> <?php echo get_the_content();?> </p>
+                                                    <a href="<?php echo the_permalink(); ?>">READ MORE</a>
+                                                </div>
+                                        <?php endwhile; ?>
+                                          
+                                     </div>
+                                           <?php    /* Restore original Post Data */
+                                                wp_reset_postdata();
+                                                endif;
+                                        ?>
+                        <div class='col-lg-12 col-sm-12 col-md-12 text-center'><h2>Customers</h2></div>
+                            <?php       
+           
+                          //get custom fields
+                            $args_custom_filed = array(
+                                                  'post_type' => 'customers',
+                                                  'posts_per_page' => 2
+                                              );
+                            $customers = new WP_Query($args_custom_filed);
+                            
+                            if ( $customers->have_posts() ) :?>
+                                  <div class='col-lg-12 col-sm-12 col-md-12'>
+                                 <?php
+                                  while ( $customers->have_posts() ) : $customers->the_post();
+                                          
+                                          ?>
+                                                <div class='col-lg-6 col-sm-6 col-md-6'>
+                                                     <?php  $img= CFS()->get( 'customer_logo',get_the_ID() );  ?> 
+                                                    <img src="<?php echo $img ?>" >                                                    
+                                                    <h4> <?php echo get_the_title();?> </h4>
+                                                    <p> <?php echo  CFS()->get( 'customer_description',get_the_ID() );?> </p>
+                                                     <?php 
+                                                     
+                                                    $loop = CFS()->get( 'customer_urls',get_the_ID() );
+                                                        foreach ( $loop as $row ) {
+                                                           //$row['url_name'];
+                                                           echo "<p>".$row['url']."</p>";
+
+                                                            }
+                                                    
+                                                     ?>
+                                                      <p><?php $term_list=  wp_get_post_terms( get_the_ID(), 'sagments' );
+                                                             foreach($term_list as $term_single) {
+                                                                echo $term_single->name.", "; //do something here
+                                                                }?>                                        
+                                                    </p>
+                                                    
+                                                </div>
+                                  <?php endwhile;?>
+                                  </div>
+                        <?php
+                                  /* Restore original Post Data */
+                                  wp_reset_postdata();
+                          endif;
+                          ?>
+                        <div class="col-md-12 col-sm-12 col-lg-12 text-center"> 
+                            <h2>Home page content</h2>
+                            <p> <?php the_content(); ?></p>
+                        </div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+ 
+                        <?php endwhile; // End of the loop. ?>
+<?php
+//get_sidebar();
+get_footer();
--- app/wp-content/themes/zype-test/header.php
+++ app/wp-content/themes/zype-test/header.php
@@ -24,7 +24,7 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'zype-test' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
+<!--		<div class="site-branding">
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -38,12 +38,12 @@
 				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
 			<?php
 			endif; ?>
-		</div><!-- .site-branding -->
+		</div> .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
+<!--		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'zype-test' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		</nav> #site-navigation 
+	</header> #masthead -->
 
 	<div id="content" class="site-content">
